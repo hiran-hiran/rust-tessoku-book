649c309407ade70543471bc63fe40c4fdf801271
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-01-30 00:32:38 +0900

    02

--- tessoku-book/src/bin/a02.rs
+++ tessoku-book/src/bin/a02.rs
@@ -1,3 +1,21 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: u16,
+        X: u16,
+        A: [u16; N],
+    }
+
+    let mut result: &str = "No";
+
+    for a in A {
+        if a == X {
+            result = "Yes"
+        }
+    }
+
+    println!("{}", result);
 }
--- tessoku-book/src/bin/b02.rs
+++ tessoku-book/src/bin/b02.rs
@@ -1,3 +1,20 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        A: u16,
+        B: u16,
+    }
+
+    let mut result: &str = "No";
+
+    for num in A..=B {
+        if 100 % num == 0 {
+            result = "Yes";
+        }
+    }
+
+    println!("{}", result);
 }
