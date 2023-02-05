c0361419103e613e3b799500d1c9246323b04488
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-02-05 10:29:15 +0900

    03

--- tessoku-book/src/bin/a03.rs
+++ tessoku-book/src/bin/a03.rs
@@ -1,3 +1,23 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: i32,
+        K: i32,
+        P: [i32; N],
+        Q: [i32; N],
+    }
+
+    let mut ans = "No";
+    for p in &P {
+        for q in &Q {
+            if p + q == K {
+                ans = "Yes";
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
--- tessoku-book/src/bin/b03.rs
+++ tessoku-book/src/bin/b03.rs
@@ -1,3 +1,24 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        A: [usize; N],
+    }
+
+    let mut ans = "No";
+
+    for i in 0..N {
+        for j in i + 1..N {
+            for k in j + 1..N {
+                if A[i] + A[j] + A[k] == 1000 {
+                    ans = "Yes";
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
