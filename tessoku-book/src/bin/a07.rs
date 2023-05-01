e50cb7939581e88334b8f0ae5d6f3923bd431b50
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-05-01 23:04:22 +0900

    07

--- tessoku-book/src/bin/a07.rs
+++ tessoku-book/src/bin/a07.rs
@@ -1,3 +1,24 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        D: usize,
+        N: usize,
+        LR: [(usize,usize); N]
+    }
+
+    let mut B = vec![0; D + 1];
+
+    for &(L, R) in &LR {
+        B[L - 1] += 1;
+        B[R] -= 1;
+    }
+
+    let mut answer = 0;
+    for b in &B[..D] {
+        answer += b;
+        println!("{:?}", answer);
+    }
 }
--- tessoku-book/src/bin/b07.rs
+++ tessoku-book/src/bin/b07.rs
@@ -1,3 +1,32 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        T: usize,
+        N: usize,
+        LR: [(usize, usize); N]
+    };
+
+    let mut B = vec![0; T + 1];
+    // println!("B: {:?}", B);
+
+    for &(L, R) in &LR {
+        B[L] += 1;
+        B[R] -= 1;
+    }
+
+    let mut answer = 0;
+    for b in &B[..T] {
+        answer += b;
+        println!("{}", answer);
+    }
+
+    // println!("B: {:?}", B);
+    // println!("B[1]: {:?}", B[1]);
+
+    // println!("T: {}", T);
+    // println!("N: {}", N);
+    // println!("LR: {:?}", LR);
 }
