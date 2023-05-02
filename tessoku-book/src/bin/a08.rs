be61dcb087afa76f2a1415a2e368992b8bce53bf
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-05-02 23:30:35 +0900

    a08

--- tessoku-book/src/bin/a08.rs
+++ tessoku-book/src/bin/a08.rs
@@ -1,3 +1,32 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        H: usize,
+        W: usize,
+        matrix: [[usize; W]; H],
+        Q: usize,
+        ABCD : [(usize, usize, usize, usize); Q]
+    };
+
+    let mut cum = vec![vec![0; W + 1]; H + 1];
+
+    for h in 1..=H {
+        for w in 1..=W {
+            cum[h][w] = cum[h][w - 1] + matrix[h - 1][w - 1];
+        }
+    }
+    for w in 1..=W {
+        for h in 1..=H {
+            cum[h][w] += cum[h - 1][w];
+        }
+    }
+
+    for &(A, B, C, D) in &ABCD {
+        let answer = cum[C][D] + cum[A - 1][B - 1] - cum[C][B - 1] - cum[A - 1][D];
+
+        println!("{}", answer);
+    }
 }
