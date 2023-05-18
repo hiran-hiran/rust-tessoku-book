ae042093f242ec7c4c4c543df3cf0a51783376aa
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-05-18 23:45:55 +0900

    26

--- tessoku-book/src/bin/a26.rs
+++ tessoku-book/src/bin/a26.rs
@@ -1,3 +1,28 @@
+use num_integer::Roots;
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        Q: usize,
+        X: [usize; Q]
+    }
+
+    fn is_prime(x: &usize) -> &str {
+        let mut answer = "Yes";
+
+        for r in 2..=x.sqrt() {
+            if x % r == 0 {
+                answer = "No";
+            }
+        }
+
+        return answer;
+    }
+
+    for &x in &X {
+        let result = is_prime(&x);
+        println!("{}", result);
+    }
 }
