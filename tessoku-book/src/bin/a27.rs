defc3211b3ada2f19edec249c24ddde7cd0a73d3
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-07-30 16:20:33 +0900

    27

--- tessoku-book/src/bin/a27.rs
+++ tessoku-book/src/bin/a27.rs
@@ -1,3 +1,21 @@
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        mut A: usize,
+        mut B: usize,
+    }
+
+    let result = get_gcd(A, B);
+
+    println!("{}", result);
+}
+
+fn get_gcd(a: usize, b: usize) -> usize {
+    if a == 0 {
+        b
+    } else {
+        get_gcd(b % a, a)
+    }
 }
--- tessoku-book/src/bin/b27.rs
+++ tessoku-book/src/bin/b27.rs
@@ -1,3 +1,22 @@
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        mut A: usize,
+        mut B: usize,
+    }
+
+    let gcd = get_gcd(A, B);
+    let result = A * B / gcd;
+
+    println!("{}", result);
+}
+
+fn get_gcd(a: usize, b: usize) -> usize {
+    if a == 0 {
+        b
+    } else {
+        get_gcd(b % a, a)
+    }
 }
