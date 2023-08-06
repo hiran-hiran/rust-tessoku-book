9d4f4b543ada708e096291b0953026e70515d379
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-08-06 15:10:17 +0900

    31

--- tessoku-book/src/bin/a31.rs
+++ tessoku-book/src/bin/a31.rs
@@ -1,3 +1,17 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N:usize
+    }
+
+    let n3 = N / 3;
+    let n5 = N / 5;
+    let n15 = N / 15;
+
+    let result = n3 + n5 - n15;
+
+    println!("{}", result);
 }
--- tessoku-book/src/bin/b31.rs
+++ tessoku-book/src/bin/b31.rs
@@ -1,3 +1,21 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N:usize
+    }
+
+    let n3 = N / 3;
+    let n5 = N / 5;
+    let n7 = N / 7;
+    let n15 = N / 15;
+    let n21 = N / 21;
+    let n35 = N / 35;
+    let n105 = N / 105;
+
+    let result = n3 + n5 + n7 - n15 - n21 - n35 + n105;
+
+    println!("{}", result);
 }
