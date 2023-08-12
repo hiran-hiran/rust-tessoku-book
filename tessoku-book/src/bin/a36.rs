a4140641eca451659d3de20d0e9069db3acea92c
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-08-13 00:51:25 +0900

    a36

--- tessoku-book/src/bin/a36.rs
+++ tessoku-book/src/bin/a36.rs
@@ -1,3 +1,19 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        K: usize
+    }
+
+    let isMinTimes = K >= N * 2 - 2;
+    let isEven = K % 2 == 0;
+
+    if isMinTimes && isEven {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
