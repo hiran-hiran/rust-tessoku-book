600f96cc9d2d926d6f5894bb757c73be81a04675
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-08-13 16:07:12 +0900

    a37

--- tessoku-book/src/bin/a37.rs
+++ tessoku-book/src/bin/a37.rs
@@ -1,3 +1,19 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        M: usize,
+        B: usize,
+        A: [usize; N],
+        C: [usize; M],
+    }
+
+    let calc_b = B * N * M;
+    let calc_a = A.iter().sum::<usize>() * M;
+    let calc_c = C.iter().sum::<usize>() * N;
+
+    println!("{}", calc_a + calc_b + calc_c);
 }
