2b0f385a5664bf1c2d43326698292fb054c047af
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-08-20 17:05:11 +0900

    a39

--- tessoku-book/src/bin/a39.rs
+++ tessoku-book/src/bin/a39.rs
@@ -1,3 +1,26 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        mut LR: [(usize, usize); N]
+    }
+
+    let mut current_time = 0;
+    let mut answer = 0;
+    // 終わるのが早い順にsort
+    LR.sort_by(|l, r| l.1.cmp(&r.1));
+    // imutable ver.
+    // let sorted_LR = LR.iter().cloned().sorted_by(|l, r| l.1.cmp(&r.1));
+
+    for (l, r) in LR {
+        if current_time <= l {
+            current_time = r;
+            answer += 1;
+        }
+    }
+
+    println!("{}", answer);
 }
